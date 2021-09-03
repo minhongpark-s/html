a7f100352addb0fbf4e31d9681d465795acd5f82
minhongpark <minhong10003@gmail.com>
2021-09-03 20:41:19 +0900

    ver 1.22 S

--- makeCanvas/realTimeMap.js
+++ makeCanvas/realTimeMap.js
@@ -3,6 +3,8 @@ var updateInterval = 1000;
 var x1=0,y1=0;
 var x2=0,y2=0;
 var x3=0,y3=0;
+var x4=0,y4=0;
+var x5=0,y5=0;
 var color="";
 
 //그림판 초기화 함수
@@ -51,13 +53,17 @@ function handleResults(data)
     drawLineTo_1(newCoords[0],newCoords[1],newCoords[2]);
     drawLineTo_2(newCoords[3],newCoords[4],newCoords[5]);
     drawLineTo_3(newCoords[6],newCoords[7],newCoords[8]);
+    drawLineTo_4(newCoords[9],newCoords[10],newCoords[11]);
+    drawLineTo_5(newCoords[12],newCoords[13],newCoords[14]);
     // 스택 증가
     if(newCoords[0]) call_stack++;
     // 반환값 확인
     div1.innerHTML += call_stack-1 +"번째 반복입니다. <br/>" 
     +"서버로부터 반환된 xy_1의 값은 X:" + newCoords[0] + " , Y: " + newCoords[1] + ", 선의 색상은: "+ newCoords[2]+ "<br/>"
     +"서버로부터 반환된 xy_2의 값은 X:" + newCoords[3] + " , Y: " + newCoords[4] + ", 선의 색상은: "+ newCoords[5]+ "<br/>"
-    +"서버로부터 반환된 xy_3의 값은 X:" + newCoords[6] + " , Y: " + newCoords[7] + ", 선의 색상은: "+ newCoords[8]+ "<br/><br/>";
+    +"서버로부터 반환된 xy_3의 값은 X:" + newCoords[6] + " , Y: " + newCoords[7] + ", 선의 색상은: "+ newCoords[8]+ "<br/>"
+    +"서버로부터 반환된 xy_4의 값은 X:" + newCoords[9] + " , Y: " + newCoords[10] + ", 선의 색상은: "+ newCoords[11]+ "<br/>"
+    +"서버로부터 반환된 xy_5의 값은 X:" + newCoords[12] + " , Y: " + newCoords[13] + ", 선의 색상은: "+ newCoords[14]+ "<br/><br/>";
     // updateInterval 후 반복.
     setTimeout("updateChart()",updateInterval);
 }
@@ -81,7 +87,7 @@ function drawLineTo_1(x,y,color)
     }
     else{
         ctx.moveTo(x1,y1);
-        ctx.lineTo(x1+10,y);
+        ctx.lineTo(x1+10,y1);
         ctx.lineWidth=2;
         ctx.stroke();
         ctx.closePath();
@@ -95,13 +101,22 @@ function drawLineTo_2(x,y,color)
     ctx.beginPath();
     if(x2==0) ctx.strokeStyle='white';
     else ctx.strokeStyle=color;
-    ctx.moveTo(x2,y2);
-    ctx.lineTo(x,y);
-    ctx.lineWidth=2;
-    ctx.stroke();
-    ctx.closePath();
-    x2=x;
-    y2=y;
+    if(x){
+        ctx.moveTo(x2,y2);
+        ctx.lineTo(x,y);
+        ctx.lineWidth=2;
+        ctx.stroke();
+        ctx.closePath();
+        x1=x;
+        y1=y;
+    }
+    else{
+        ctx.moveTo(x2,y2);
+        ctx.lineTo(x2+10,y2);
+        ctx.lineWidth=2;
+        ctx.stroke();
+        ctx.closePath();
+    }
 }
 
 function drawLineTo_3(x,y,color)
@@ -111,11 +126,70 @@ function drawLineTo_3(x,y,color)
     ctx.beginPath();
     if(x3==0) ctx.strokeStyle='white';
     else ctx.strokeStyle=color;
-    ctx.moveTo(x3,y3);
-    ctx.lineTo(x,y);
-    ctx.lineWidth=2;
-    ctx.stroke();
-    ctx.closePath();
-    x3=x;
-    y3=y;
+    if(x){
+        ctx.moveTo(x3,y3);
+        ctx.lineTo(x,y);
+        ctx.lineWidth=2;
+        ctx.stroke();
+        ctx.closePath();
+        x1=x;
+        y1=y;
+    }
+    else{
+        ctx.moveTo(x3,y3);
+        ctx.lineTo(x3+10,y3);
+        ctx.lineWidth=2;
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
+
+function drawLineTo_4(x,y,color)
+{
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    if(x4==0) ctx.strokeStyle='white';
+    else ctx.strokeStyle=color;
+    if(x){
+        ctx.moveTo(x4,y4);
+        ctx.lineTo(x,y);
+        ctx.lineWidth=2;
+        ctx.stroke();
+        ctx.closePath();
+        x4=x;
+        y4=y;
+    }
+    else{
+        ctx.moveTo(x4,y4);
+        ctx.lineTo(x4+10,y4);
+        ctx.lineWidth=2;
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
+
+function drawLineTo_5(x,y,color)
+{
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    if(x5==0) ctx.strokeStyle='white';
+    else ctx.strokeStyle=color;
+    if(x){
+        ctx.moveTo(x5,y5);
+        ctx.lineTo(x,y);
+        ctx.lineWidth=2;
+        ctx.stroke();
+        ctx.closePath();
+        x5=x;
+        y5=y;
+    }
+    else {
+        ctx.moveTo(x5,y5);
+        ctx.lineTo(x5+10,y5);
+        ctx.lineWidth=2;
+        ctx.stroke();
+        ctx.closePath();
+    }
 }
